abf96929e2ff8340daf3eba19c7359b1341569fa
HesamJafarian <hesamjafarian@gmail.com>
2024-10-04 21:41:48 +0300

    Add data tranformation pipeline

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from SSVEPA import logger
 from SSVEPA.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from SSVEPA.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from SSVEPA.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 
 logger.info("Welcome to SSVEPA Project")
@@ -29,3 +30,15 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+STAGE_NAME = "Data Transformation"
+
+try:
+    logger.info(f">>>>>> Stage {STAGE_NAME} Started")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(f">>>>>> Stage {STAGE_NAME} Finished")
+
+except Exception as e:
+    logger.error(f"An error occurred during {STAGE_NAME} Stage!", exc_info=True)
+    logger.exception(e)
+    raise e
